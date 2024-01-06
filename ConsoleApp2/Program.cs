6be1cbaef2c18a20d5d3870a2be3a278efce2f87
iFubuk <102698120+iFubuk@users.noreply.github.com>
2024-01-06 20:46:43 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Pow(a - b, 2));
+        }
+    }
+}
